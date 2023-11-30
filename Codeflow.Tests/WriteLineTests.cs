c3f6298911debe1c382bd96c8962f14ea518a584
Stephen Weatherhead <stephenw42@outlook.com>
2023-11-30 22:10:39 +0000

    Expression with arguments

--- Codeflow.Tests/WriteLineTests.cs
+++ Codeflow.Tests/WriteLineTests.cs
@@ -20,5 +20,21 @@ namespace System.Activities
             Assert.Single(l_Sequence.Activities);
             Assert.NotNull(l_WriteLine.Text);
         }
+        [Fact]
+        public void WriteLineWithExpression()
+        {
+            // arrange
+            Variable<string> l_MyVariable = new Variable<string>("MyVariable");
+            WorkflowBuilder l_Builder = new WorkflowBuilder();
+            l_Builder.WriteLine(e => l_MyVariable.Get(e));
+
+            // act
+            Sequence l_Sequence = (Sequence)l_Builder.GetActivity();
+            WriteLine l_WriteLine = (WriteLine)l_Sequence.Activities.First();
+
+            // assert
+            Assert.Single(l_Sequence.Activities);
+            Assert.NotNull(l_WriteLine.Text);
+        }
     }
 }

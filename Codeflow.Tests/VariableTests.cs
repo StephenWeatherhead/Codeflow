8611ada7cf2ee6616b1e5ffc2e6daa60b7cb79ac
Stephen Weatherhead <stephenw42@outlook.com>
2023-12-18 15:25:04 +0000

    Variable with no default value unit test

---
+++ Codeflow.Tests/VariableTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Activities
+{
+    public class VariableTests
+    {
+        [Fact]
+        public void VariableWithNoDefaultValue()
+        {
+            // arrange
+            WorkflowBuilder l_Builder = new WorkflowBuilder();
+            l_Builder.Variable<string>("Test");
+
+            // act
+            Sequence l_Sequence = (Sequence)l_Builder.GetActivity();
+            Variable l_Variable = l_Sequence.Variables[0];
+
+            // assert
+            Assert.Single(l_Sequence.Variables);
+            Assert.Equal("Test", l_Variable.Name);
+            Assert.Equal(typeof(string), l_Variable.Type);
+        }
+    }
+}

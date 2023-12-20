3bb5cf0fa97e4f0a9c63b29cf135ea9373e2d659
Stephen Weatherhead <stephenw42@outlook.com>
2023-12-20 11:09:37 +0000

    unit test for variable default value

--- Codeflow.Tests/VariableTests.cs
+++ Codeflow.Tests/VariableTests.cs
@@ -25,5 +25,24 @@ namespace System.Activities
             Assert.Equal("Test", l_Variable.Name);
             Assert.Equal(typeof(string), l_Variable.Type);
         }
+
+        [Fact]
+        public void VariableWithDefaultValue()
+        {
+            // arrange
+            WorkflowBuilder l_Builder = new WorkflowBuilder();
+            l_Builder.Variable<string>("Test", "hello");
+
+            // act
+            Sequence l_Sequence = (Sequence)l_Builder.GetActivity();
+            Variable l_Variable = l_Sequence.Variables[0];
+            string l_DefaultValue = (string)WorkflowInvoker.Invoke(l_Variable.Default)["Result"];
+
+            // assert
+            Assert.Single(l_Sequence.Variables);
+            Assert.Equal("Test", l_Variable.Name);
+            Assert.Equal("hello", l_DefaultValue);
+            Assert.Equal(typeof(string), l_Variable.Type);
+        }
     }
 }

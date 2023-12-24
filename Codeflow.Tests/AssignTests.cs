cba877766ea70fb0c7d8c49818fa6e588ed8614e
Stephen Weatherhead <stephenw42@outlook.com>
2023-12-24 11:11:55 +0000

    add assign unit test for variable-expression overload

--- Codeflow.Tests/AssignTests.cs
+++ Codeflow.Tests/AssignTests.cs
@@ -9,12 +9,30 @@ namespace System.Activities
 {
     public class AssignTests
     {
+        [Fact]
         public void AssignVariableWithValue()
         {
             // arrange
             WorkflowBuilder l_Builder = new WorkflowBuilder();
             Variable l_Variable = l_Builder.Variable<string>("Test");
             l_Builder.Assign<string>(l_Variable, "Test");
+
+            // act
+            Sequence l_Sequence = (Sequence)l_Builder.GetActivity();
+            Assign l_Assign = (Assign)l_Sequence.Activities.First();
+
+            // assert
+            Assert.NotNull(l_Assign.To);
+            Assert.NotNull(l_Assign.Value);
+        }
+
+        [Fact]
+        public void AssignVariableWithExpression()
+        {
+            // arrange
+            WorkflowBuilder l_Builder = new WorkflowBuilder();
+            Variable l_Variable = l_Builder.Variable<int>("Test");
+            l_Builder.Assign<int>(l_Variable, e => Random.Shared.Next());
 
             // act
             Sequence l_Sequence = (Sequence)l_Builder.GetActivity();

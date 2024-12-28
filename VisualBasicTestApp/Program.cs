512fe28e81a5ad5a0d5fd8f72697db571973628e
Stephen Weatherhead <stephenw42@outlook.com>
2024-12-28 18:40:51 +0000

    Added code to get overall parameters in VB PoC

--- VisualBasicTestApp/Program.cs
+++ VisualBasicTestApp/Program.cs
@@ -3,6 +3,9 @@ using System.Xaml;
 using System.IO;
 using System.Xml;
 using Codeflow.TestResources;
+using System.Xaml.Schema;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 namespace VisualBasicProofOfConcept
 {
@@ -15,16 +18,27 @@ namespace VisualBasicProofOfConcept
             Console.WriteLine("In our VB proof of concept, we're targeting the WriteLine Text property");
             Console.WriteLine("so our return type will be String. The challenge is detecting the ");
             Console.WriteLine("parameter names, parameter types, and the expression itself.");
+            Console.WriteLine();
+            // parse workflow
+            string expression;
+            Dictionary<string, XamlType> parameters = new Dictionary<string, XamlType>();
             using (XamlXmlReader xmlReader = new XamlXmlReader(new StringReader(xamlString)))
             {
                 MyXamlObject? currentObject = null;
                 MyXamlMember? currentMember = null;
                 Stack<MyXamlObject> objectStack = new Stack<MyXamlObject>();
                 Stack<MyXamlMember> memberStack = new Stack<MyXamlMember>();
+                List<NamespaceDeclaration> namespaces = new List<NamespaceDeclaration>();
                 while (xmlReader.Read())
                 {
                     switch (xmlReader.NodeType)
                     {
+                        case XamlNodeType.NamespaceDeclaration:
+                            if (xmlReader.Namespace != null)
+                            {
+                                namespaces.Add(xmlReader.Namespace);
+                            }
+                            break;
                         case XamlNodeType.StartObject:
                             currentObject = new MyXamlObject();
                             currentObject.XamlType = xmlReader.Type;
@@ -76,10 +90,35 @@ namespace VisualBasicProofOfConcept
                     .First(m => m.IsContent)
                     .Content.First(xo => xo.XamlType.Name == "WriteLine");
                 var writeLineText = writeLine.Members.First(m => m.Name == "Text").Value;
-                string expression = writeLineText.Substring(1, writeLineText.Length - 2);
-                Console.WriteLine("The WriteLine expression is : " + expression);
+                expression = writeLineText.Substring(1, writeLineText.Length - 2);
+                // get overall parameters
+                Dictionary<string, XamlType> overallParameters = new Dictionary<string, XamlType>();
+                var activityProperties = currentObject.Members.First(m => m.Name == "Members").Content;
+                foreach(var propertyObject in activityProperties)
+                {
+                    string propertyName = propertyObject.Members.First(m => m.Name == "Name").Value;
+                    string typeString = propertyObject.Members.First(m => m.Name == "Type").Value;
+                    XamlType propertyType = GetXamlType(typeString, namespaces, xmlReader.SchemaContext)
+                        .TypeArguments.First();
+                    overallParameters.Add(propertyName, propertyType);
+                }
+                // get sequence parameters
+                // detect parameters in expression
             }
+            Console.WriteLine(VBHelper.GenerateVisualBasicMethod("GetWriteLineExpression", new XamlType(typeof(string), new XamlSchemaContext()), parameters, expression));
             Console.ReadLine();
+        }
+
+        private static XamlType GetXamlType(string typeName, List<NamespaceDeclaration> namespaces, XamlSchemaContext context)
+        {
+            XamlTypeName xamlTypeName = XamlTypeName.Parse(typeName, new SimpleXamlNamespaceResolver(namespaces));
+            return GetXamlType(xamlTypeName, context);
+        }
+        private static XamlType GetXamlType(XamlTypeName xamlTypeName, XamlSchemaContext context)
+        {
+            return new XamlType(xamlTypeName.Namespace, xamlTypeName.Name,
+                xamlTypeName.TypeArguments.Select(tn => GetXamlType(tn, context)).ToList(),
+                context);
         }
     }
 }
---
+++ VisualBasicTestApp/SimpleXamlNamespaceResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+using System.Xml.Linq;
+
+namespace VisualBasicProofOfConcept
+{
+    internal class SimpleXamlNamespaceResolver(List<NamespaceDeclaration> namespaces) : IXamlNamespaceResolver
+    {
+        public string GetNamespace(string prefix)
+        {
+            return namespaces.FirstOrDefault(space =>
+            string.Equals(space.Prefix, prefix, StringComparison.InvariantCultureIgnoreCase))
+                .Namespace;
+        }
+
+        public IEnumerable<NamespaceDeclaration> GetNamespacePrefixes()
+        {
+            return namespaces;
+        }
+    }
+}

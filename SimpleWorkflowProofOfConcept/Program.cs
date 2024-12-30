e0174587a33558be1ca100524a881ff3af1aa9f3
Stephen Weatherhead <stephenw42@outlook.com>
2024-12-30 11:09:57 +0000

    Simple workflow PoC inital project

---
+++ SimpleWorkflowProofOfConcept/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleWorkflowProofOfConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // parse workflow
+            // print workflow c#
+            // print workflow vb
+            // run compiled workflow
+        }
+    }
+}
